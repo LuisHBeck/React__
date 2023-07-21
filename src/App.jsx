bebd95f6edd2b7ffca192acf9d551ee5cacba04c
LuisHBeck <beck.henrique.luis@gmail.com>
2023-07-21 20:48:57 -0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const name = <h1>Luís Beck</h1>;
+  return (
+    <div className="App">
+      {name}
+      {name}
+      {name}
+    </div>  
+  );
+}
+
+export default App;
