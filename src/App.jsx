1df0bfe87a5a9134b73ccf3c56eb763b44e68bd6
LuisHBeck <beck.henrique.luis@gmail.com>
2023-07-23 20:24:08 -0300

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,55 @@
 import styles from './App.module.css';
-import { User } from './components/User';
+import { useState } from 'react';
+import { Task } from './components/Task';
 
 function App() {
-  const users = [
-    {name: 'Pedro', age: 21},
-    {name: 'Luís', age: 19},
-    {name: 'Daniel', age: 50},
-  ];
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const length = todoList.length; 
+    const task = { 
+      id: length === 0 ? 1 : todoList[length - 1].id + 1,
+      taskName: newTask,
+      isCompleted: false
+    }
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (taskId) => {
+    const newList = todoList.filter((task)=>{
+      return task.id !== taskId;
+    });
+
+    setTodoList(newList)
+  };
+
+  const completeTask = (taskId) => {
+    setTodoList(todoList.map((task) => {
+      if (task.id === taskId) {
+        return {...task, isCompleted: true};
+      }else {
+        return task;
+      }
+    }));
+  };
 
   return (
     <div className={styles.App}>
-      {users.map((user, key) => {
-        return (
-        <User name={user.name} age={user.age}/>
-        )
-      })}
+      <div className={styles.addTask}>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <div className={styles.list}>
+        {todoList.map((task) => {
+          return <Task taskName={task.taskName} id={task.id} deleteTask={deleteTask} completeTask={completeTask} completed={task.isCompleted}/>
+        })}
+      </div>
     </div>
   )
 }
