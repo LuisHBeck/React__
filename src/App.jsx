5a95156cc76a16a440b8c738d7dbe5cea0ba32a8
LuisHBeck <beck.henrique.luis@gmail.com>
2023-08-28 11:30:22 -0300

    feat: ternary func and list

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  const age = 19;
+  const isGreen = false;
+
+  const names = ['Peter', 'Jake', 'Mike', 'Dustin', 'Carl'];
+
+  const users = [
+    { name: 'Peter', age:25 },
+    { name: 'Jake', age:15 },
+    { name: 'Carl', age:19 }
+  ]
+
   return (
     <div className="App">
-      <Job salary={90000} position="Senior SDE" company="Amazon"/>
+      {/*
+      <h2>{age >= 18 ? "Over age" : "Under age"}</h2>
+      <h1 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR</h1>
+      { isGreen && <button>Here</button> } */}
+
+      {/* {names.map((name, key) => {
+        return <h2 key={key}>{ name }</h2>;
+      })} */}
+
+      { users.map((user, key) => {
+        return (
+          <User name={user.name} age={user.age} />
+        )
+      }) }
+
     </div>
   )
 }
 
-const Job = (props) => {
+const User = (props) => {
   return (
     <div>
-      <h1>{ props.salary }</h1>
-      <h1>{ props.position }</h1>
-      <h1>{ props.company }</h1>
+      <h1>{ props.name } { props.age }</h1>
     </div>
   )
 }
