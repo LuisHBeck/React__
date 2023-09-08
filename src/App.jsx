0f4f7a4d6c3cbb61391791c349d90d134fdff888
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-08 11:07:10 -0300

    feat: reactQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { Home } from './pages/home';
-import { Menu } from './pages/Menu';
-import { Contact } from './pages/Contact';
-import { Error } from './pages/Error';
-import { Navbar } from './components/Navbar';
+import { Home } from "./pages/home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Error } from "./pages/Error";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-    return (
-        <div className='App'>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path='/' element={<Home />}/>
-                    <Route path='/menu' element={<Menu />}/>
-                    <Route path='/contact' element={<Contact />}/>
-                    <Route path='*' element={<Error />}/>
-                </Routes>
-            </Router>
-        </div> 
-    )
+	const client = new QueryClient({
+		defaultOptions: {
+			queries: {
+				refetchOnWindowFocus: true,
+			},
+		},
+	});
+
+	return (
+		<div className="App">
+			<QueryClientProvider client={client}>
+				<Router>
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/menu" element={<Menu />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="*" element={<Error />} />
+					</Routes>
+				</Router>
+			</QueryClientProvider>
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 export const Home = () => {
-    return <h1> THIS IS THE HOME PAGE </h1>  
-}
+	const {
+		data: catFact,
+		isLoading,
+		refetch,
+	} = useQuery(["cat"], async () => {
+		try {
+			const response = await axios.get("https://catfact.ninja/fact");
+			return response.data;
+		} catch (err) {
+			console.log(err);
+			return null;
+		}
+	});
+
+	// const { data, isLoading, refetch } = useQuery(["cat"], () => {
+	// 	return axios.get("https://catfact.ninja/fact/").then((response) => response.data);
+	// });
+
+	if (isLoading) {
+		return <h1>Loading...</h1>;
+	}
+
+	return (
+		<h1>
+			THIS IS THE HOME PAGE
+			<p>{catFact?.fact}</p>
+			<button onClick={refetch}> Refetch </button>
+		</h1>
+	);
+};
