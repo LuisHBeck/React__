23434dfbebb8c07316369b6ff80c57d87ee7fbf9
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-08 21:17:38 -0300

    feat: props type

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
-import { Counter } from "./components/Counter";
+import { Person } from "./components/Person";
 
 function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  )
+	return (
+		<div className="App">
+			<Person
+				name="Beck"
+				email="beck@gmail.com"
+				age={19}
+				isMarried={false}
+				friends={["Torres", "Pedrinho", "Alemão"]}
+			/>
+		</div>
+	);
 }
 
 export default App;
